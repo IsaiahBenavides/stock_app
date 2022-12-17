a7448d60b8aedfa1f57975a597d1385ce00a1b2e
Isaiah Bens <benavidesisaiah@outlook.com>
2022-12-17 13:28:37 -0500

    Stock details page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,8 @@
 export default function About() {
-    return(
-        <p>	"This is the About page"</p>
-    )
+  return (
+    <>
+      <p> "This is the About page"</p>
+      <p>This is a static stocks page, showing my capabilities to code a simple SPA</p>
+    </>
+  );
 }
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,5 +1,35 @@
+import data from "../data";
+
 export default function Stock() {
-    return(
-        <p> A single stock</p>
-    )
+  return (
+    <>
+      {data.map((stock, i) => {
+        if (`/stocks/:${stock.symbol}` === window.location.pathname) {
+          return (
+            <>
+                <h3>{data[i].name}</h3>
+              <table>
+                <tr>
+                  <th>Symbol</th>
+                  <th>lastPrice</th>
+                  <th>Change</th>
+                  <th>High</th>
+                  <th>Low</th>
+                  <th>Open</th>
+                </tr>
+                <tr>
+                  <th>{data[i].symbol}</th>
+                  <th>{data[i].lastPrice}</th>
+                  <th>{data[i].change}</th>
+                  <th>{data[i].high}</th>
+                  <th>{data[i].low}</th>
+                  <th>{data[i].open}</th>
+                </tr>
+              </table>
+            </>
+          );
+        }
+      })}
+    </>
+  );
 }
